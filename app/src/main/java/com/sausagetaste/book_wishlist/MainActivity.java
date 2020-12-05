77c258ebb18c9bf03713e52d574741e51abc1112
Sungmin Woo <woos8899@gmail.com>
2020-12-05 22:46:52 +0900

    Open dialog on clicking fab

--- app/src/main/java/com/sausagetaste/book_wishlist/MainActivity.java
+++ app/src/main/java/com/sausagetaste/book_wishlist/MainActivity.java
@@ -1,14 +1,19 @@
 package com.sausagetaste.book_wishlist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Vector;
 
@@ -37,6 +42,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class OnClickFAB implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogTheme);
+            builder.setTitle("Title");
+
+            final EditText input = new EditText(MainActivity.this);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            builder.setView(input);
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    final String input_text = input.getText().toString();
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
+        }
+
+    }
+
 
     // Attributes
 
@@ -59,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
         ListView lv = (ListView) this.findViewById(R.id.book_list_view);
         lv.setOnItemClickListener(new BookListView_OnClickListener(this));
         lv.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new OnClickFAB());
 
         for (int i = 0; i < 10000; ++i) {
             this.list_item_texts.add("사람 " + Integer.toString(i));
